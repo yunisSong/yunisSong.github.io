5ebe76750365f7b3ef706fb70279593dfa2f4ac0
Yunis <songshangyong@Y.local>
2020-01-13 09:18:52 +0800

    禁止响应鼠标滚动事件

--- js/yunisPic.js
+++ js/yunisPic.js
@@ -8,7 +8,8 @@ $(function() {
              $("#SSYLargeImage").attr({ "src": $(this).attr("src") });
 
             //禁止响应鼠标滚动事件
-            $(document).bind('mousewheel', function(event, delta) {return false;});
+            // $(document).bind('mousewheel', function(event, delta) {return false;});
+            $("html,body").addClass("none-scroll");//下层不可滑动
 
        }else {
             $("#YunisTestImage").removeClass('SYYImageShow');
@@ -16,8 +17,8 @@ $(function() {
             $("#SSYLargeImage").attr({ "src": "" });
 
             //恢复响应鼠标滚动事件
-            $(document).unbind('mousewheel');
-
+            // $(document).unbind('mousewheel');
+            $("html,body").removeClass("none-scroll");
 
        }
         });
@@ -27,7 +28,8 @@ $(function() {
         $("#YunisTestImage").addClass('SYYImageDiss');
         $("#SSYLargeImage").attr({ "src": "" });
         //恢复响应鼠标滚动事件
-        $(document).unbind('mousewheel');
+        // $(document).unbind('mousewheel');
+        $("html,body").removeClass("none-scroll");
 
     });
    
