66bc5adc472336eb9e3223862c8ed30300983d31
Yunis <songshangyong@Y.local>
2020-03-11 09:20:16 +0800

    发哨子的人

--- js/yunisPic.js
+++ js/yunisPic.js
@@ -1,28 +1,28 @@
 $(function() {
     $('p img').click(function () {
-            let imageStr = document.querySelector("#YunisTestImage").getAttribute("class");
-            console.log(`${imageStr}`);
+        let imageStr = document.querySelector("#YunisTestImage").getAttribute("class");
+        console.log(`${imageStr}`);
         if (`${imageStr}` == "SYYImageDiss") {
-             $("#YunisTestImage").removeClass('SYYImageDiss');
-             $("#YunisTestImage").addClass('SYYImageShow');
-             $("#SSYLargeImage").attr({ "src": $(this).attr("src") });
+            $("#YunisTestImage").removeClass('SYYImageDiss');
+            $("#YunisTestImage").addClass('SYYImageShow');
+            $("#SSYLargeImage").attr({ "src": $(this).attr("src") });
 
             //禁止响应鼠标滚动事件
             // $(document).bind('mousewheel', function(event, delta) {return false;});
             $("html,body").addClass("none-scroll");//下层不可滑动
 
-       }else {
-            $("#YunisTestImage").removeClass('SYYImageShow');
-            $("#YunisTestImage").addClass('SYYImageDiss');
-            $("#SSYLargeImage").attr({ "src": "" });
+        }else {
+                $("#YunisTestImage").removeClass('SYYImageShow');
+                $("#YunisTestImage").addClass('SYYImageDiss');
+                $("#SSYLargeImage").attr({ "src": "" });
 
-            //恢复响应鼠标滚动事件
-            // $(document).unbind('mousewheel');
-            $("html,body").removeClass("none-scroll");
+                //恢复响应鼠标滚动事件
+                // $(document).unbind('mousewheel');
+                $("html,body").removeClass("none-scroll");
 
-       }
-       
-       $(document).bind('mousewheel', function(event, delta) {return false;});
+        }
+    
+        $(document).bind('mousewheel', function(event, delta) {return false;});
 
         });
 
