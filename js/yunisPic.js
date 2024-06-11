da3a7465ba004880adfaa5b5d0f1d61940820943
shang_yong <>
2024-06-11 18:44:39 +0800

    添加 图片浏览 上一张 下一张 功能。

--- js/yunisPic.js
+++ js/yunisPic.js
@@ -43,6 +43,44 @@ $(function () {
   $("#YunisTestImage").click(function () {
     removeBigPic();
   });
+
+  function getAllImageSrc() {
+    let imgArr = [];
+    $("p img").each(function () {
+      imgArr.push($(this).attr("src"));
+    });
+    return imgArr;
+  }
+  $(".leftButton").click(function (e) {
+    e.stopPropagation();
+    // 获取当前显示图片的 index
+    let currentImgIndex = getAllImageSrc().indexOf(
+      $("#SSYLargeImage").attr("src")
+    );
+    // 获取上一张图片，如果当前图片为第一张图片，则返回最后一张图片
+    let prevImg =
+      currentImgIndex === 0
+        ? getAllImageSrc()[getAllImageSrc().length - 1]
+        : getAllImageSrc()[currentImgIndex - 1];
+
+    $("#SSYLargeImage").attr({ src: prevImg });
+  });
+
+  $(".rightButton").click(function (e) {
+    e.stopPropagation();
+    // 获取当前显示图片的 index
+    let currentImgIndex = getAllImageSrc().indexOf(
+      $("#SSYLargeImage").attr("src")
+    );
+    // 获取下一张图片，如果当前图片为最后一张图片，则返回第一张图片
+    let nextImg =
+      currentImgIndex === getAllImageSrc().length - 1
+        ? getAllImageSrc()[0]
+        : getAllImageSrc()[currentImgIndex + 1];
+    $("#SSYLargeImage").attr({ src: nextImg });
+
+    console.log("rightButton stop");
+  });
   $(document).keydown(function (event) {
     //判断当event.keyCode 为37时（即左方面键），执行函数to_left();
     //判断当event.keyCode 为39时（即右方面键），执行函数to_right();
