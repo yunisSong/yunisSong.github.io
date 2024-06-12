6afd2804ffa1a3d16c7bcf5154b5fa34c19ae205
shang_yong <>
2024-06-12 10:52:14 +0800

    大图浏览响应键盘事件

--- js/showBigPicture.js
+++ js/showBigPicture.js
@@ -1,23 +1,29 @@
 $(function () {
-  function mouseWheel(e) {
-    e.preventDefault();
-  }
+  // 表示大图是否在显示的状态
+  let isShowBigPic = false;
+  // 大图响应时 禁止滚动事件
+  document.addEventListener(
+    "wheel",
+    (e) => {
+      if (!isShowBigPic) {
+        return;
+      }
+      e.preventDefault();
+    },
+    { passive: false }
+  );
 
+  // 为图片添加点击事件
   $("p img").click(function () {
-    let imageStr = document
-      .querySelector("#YunisTestImage")
-      .getAttribute("class");
-    console.log(`${imageStr}`);
-    $("#SSYLargeImage").attr({ src: "" });
-    if (`${imageStr}` == "SYYImageDiss") {
-      showBigImage($(this).attr("src"));
-      //   $(this).onmousewheel = MouseWheel;
-      //   $(document).off("mousewheel", "**");
-    } else {
+    // 如果大图已经显示隐藏大图，如果大图没有显示，显示大图
+    if (isShowBigPic) {
       removeBigPic();
+    } else {
+      showBigImage($(this).attr("src"));
     }
   });
 
+  // 显示大图片
   function showBigImage(imgPath) {
     $("#YunisTestImage").removeClass("SYYImageDiss");
     $("#YunisTestImage").addClass("SYYImageShow");
@@ -26,9 +32,11 @@ $(function () {
     $("#SSYLargeImage").removeClass("animate-scale0");
 
     $("html,body").addClass("none-scroll"); //下层不可滑动
-    document.addEventListener("wheel", mouseWheel, { passive: false });
+    // document.addEventListener("wheel", mouseWheel, { passive: false });
+    isShowBigPic = true;
   }
 
+  // 隐藏大图片
   function removeBigPic() {
     $("#YunisTestImage").removeClass("SYYImageShow");
     $("#YunisTestImage").addClass("SYYImageDiss");
@@ -37,13 +45,16 @@ $(function () {
     $("html,body").removeClass("none-scroll");
 
     // $(document).unbind("mousewheel");
-    document.removeEventListener("wheel", mouseWheel, { passive: false });
+    // document.removeEventListener("wheel", mouseWheel, { passive: false });
+    isShowBigPic = false;
   }
 
+  // 为容器添加点击事件
   $("#YunisTestImage").click(function () {
     removeBigPic();
   });
 
+  // 获取全部要显示的图片
   function getAllImageSrc() {
     let imgArr = [];
     $("p img").each(function () {
@@ -51,6 +62,7 @@ $(function () {
     });
     return imgArr;
   }
+  // 上一个按钮点击事件
   $(".leftButton").click(function (e) {
     e.stopPropagation();
     // 获取当前显示图片的 index
@@ -66,6 +78,7 @@ $(function () {
     $("#SSYLargeImage").attr({ src: prevImg });
   });
 
+  // 下一张按钮点击事件
   $(".rightButton").click(function (e) {
     e.stopPropagation();
     // 获取当前显示图片的 index
@@ -78,15 +91,29 @@ $(function () {
         ? getAllImageSrc()[0]
         : getAllImageSrc()[currentImgIndex + 1];
     $("#SSYLargeImage").attr({ src: nextImg });
-
-    console.log("rightButton stop");
   });
+  // 键盘响应事件
   $(document).keydown(function (event) {
-    //判断当event.keyCode 为37时（即左方面键），执行函数to_left();
-    //判断当event.keyCode 为39时（即右方面键），执行函数to_right();
+    if (!isShowBigPic) {
+      return;
+    }
+
+    // 阻止默认事件传递
+    event.preventDefault();
+
     if (event.keyCode == 27) {
       //do somethings;
       removeBigPic();
+    }
+    // 方向键 ←  ↑ ，显示上一张图片
+    else if (event.keyCode == 37 || event.keyCode == 38) {
+      $(".leftButton").click();
+      //do somethings;
+    }
+    // 方向键 →  ↓，显示下一张图片
+    else if (event.keyCode == 39 || event.keyCode == 40) {
+      $(".rightButton").click();
+      //do somethings;
     }
   });
 });
